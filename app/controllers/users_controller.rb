0711a4c216eaeb3547ce7a183cd8fb75b4838d66
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-07-19 01:57:43 +0000

    [Start]応用課題8b　controller_7日分の投稿数

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,13 @@ class UsersController < ApplicationController
     @book_yesterday = @books.created_yesterday
     @book_this_week = @books.created_this_week
     @book_last_week = @books.created_last_week
+    
+    @book_2days_ago = @books.created_2day_ago
+    @book_3days_ago = @books.created_3day_ago
+    @book_4days_ago = @books.created_4day_ago
+    @book_5days_ago = @books.created_5day_ago
+    @book_6days_ago = @books.created_6day_ago
+    @book_7days_ago = @books.created_7day_ago
   end
 
   def index
